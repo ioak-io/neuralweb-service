be3821e3ca1a5988cf5e2b917a6e63b705d598c3
Arunkumar Selvaraj <arun@ioak.io>
2023-04-16 12:01:29 +0530

    expose an endpoint to trigger training on a space

--- src/modules/hello/helper.ts
+++ src/modules/hello/helper.ts
@@ -7,7 +7,10 @@ import { isEmptyOrSpaces } from "../../lib/Utils";
 const AI_API = process.env.AI_API || "http://localhost:5003/api";
 
 export const train_similarity_model = async (space: string) => {
+  console.log("admin training started");
   await NoteHelper._ai_train(space);
+  console.log("admin populate started");
   await NoteHelper._ai_populate(space);
+  console.log("admin training and populate finished");
   return { "status": "success" };
 };
